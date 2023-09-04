c833bc64f3642e9fb1309429e850862970015c9f
RodrigoEzequiel <rodrigocubilla87@gmail.com>
2023-09-04 09:42:41 -0300

    subi clase Domicilio

--- src/temas/t14_composicion/Domicilio.java
+++ src/temas/t14_composicion/Domicilio.java
@@ -1,6 +1,57 @@
 package temas.t14_composicion;
 
 public class Domicilio {
-    //TODO: AGREGAR TODOS LOS ATRIBUTOS DE DOMICILIO
-    //TODO: AGREGAR CONSTRUCTORES, GETTERS, SETTERS Y TOSTRING
+    private String calle;
+    private String altura;
+    private String codPos;
+    private String ciudad;
+    
+    public Domicilio(){
+    }
+
+    public Domicilio(String calle, String altura, String codPos, String ciudad) {
+        this.calle = calle;
+        this.altura = altura;
+        this.codPos = codPos;
+        this.ciudad = ciudad;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getCodPos() {
+        return codPos;
+    }
+
+    public void setCodPos(String codPos) {
+        this.codPos = codPos;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Domicilio{" + "calle:" + calle + ", altura:" + altura + ", codPos:" + codPos + ", ciudad:" + ciudad + '}';
+    }
+    
+    
 }
