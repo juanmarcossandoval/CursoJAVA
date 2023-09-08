37f533e55476b98fe4255856cfcad59f6c98f04a
Rodrigo Ezequiel <rodrigocubilla87@gmail.com>
2023-09-08 11:56:31 -0300

    fixeo de imports

--- src/temas/t15_usandoLibrerias/EstudianteLombok.java
+++ src/temas/t15_usandoLibrerias/EstudianteLombok.java
@@ -1,9 +1,7 @@
 package temas.t15_usandoLibrerias;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
